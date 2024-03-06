e4876582b9ad4f7407d17ef2518cd291bcf55837
某亚瑟 <mouyase@qq.com>
2024-03-06 17:16:35 +0800

    feat: 修改脚本和key

--- 3.sign_img_with_custom_keys.sh
+++ 3.sign_img_with_custom_keys.sh
@@ -2,8 +2,8 @@
 python3 avbtool.py add_hash_footer \
     --image apatch_boot.img \
     --partition_name boot \
-    --partition_size 67108864 \
-    --key rsa4096_vbmeta.pem \
+    --key rsa4096.pem \
     --algorithm SHA256_RSA4096 \
+    --partition_size 67108864 \
     --prop com.android.build.boot.fingerprint:UNISOC/ums512_lwfq_1h10_Natv/ums512_1h10:11/RP1A.201005.001/10338:user/release-keys \
-    --prop com.android.build.boot.os_version:11
+    --prop com.android.build.boot.os_version:11 
